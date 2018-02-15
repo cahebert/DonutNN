12090c7f94160a177fc3205143ccfac233eae036
Claire Hebert <chebert@stanford.com>
2018-02-15 11:43:56 -0800

    cleaning up and adding test predictions

--- DonutNet.py
+++ DonutNet.py
@@ -48,7 +48,7 @@ iters = args.iters
 #how many examples to test at the end
 Ntest = 100
 #batch size for each training set
-batch_size = 50
+batch_size = 25
 #using one or two input images per example
 inputs=args.inputs
 #save parameters and outputs?
@@ -63,63 +63,80 @@ mask = bool(args.mask)
 
 
 def main():
-      print 'reading data...'
-      data = pd.read_pickle(filename)
-      #data = data.head(500)
+    print 'reading data...'
+    data = pd.read_pickle(filename)
 
-      if 'd' in [z for x,y,z in layers]:
-          print 'building cnn with deconvolution... {} layers, learning rate = {}, {} iters'.format(len(layers),learning_rate,iters)
+    test_data = data.sample(Ntest,axis=0)
+    train_data = data.drop(test_data.index).reset_index(drop=True)   
+    test_data = test_data.reset_index(drop=True)
 
-          name = '%s_deconv_%dlayer_%inputs_%lr_%iters' % (os.path.splitext(os.path.basename(filename))[0],len(layers), inputs, learning_rate, iters)
-      else:
-          print 'building all convolutional cnn... {} layers, learning rate = {}, {} iters'.format(len(layers),learning_rate,iters)
+    train_test_data = train_data.sample(Ntest,axis=0).reset_index(drop=True)     
 
-          name = '%s_conv_%dlayer_%inputs_%lr_%iters' % (os.path.splitext(os.path.basename(filename))[0],len(layers), inputs, learning_rate, iters)
+    if 'd' in [z for x,y,z in layers]:
+        print 'building cnn with deconvolution... {} layers, learning rate = {}, {} iters'.format(len(layers),learning_rate,iters)
 
-      model = CNN(layers, name, learning_rate, batch_size, mask, inputs)
+        name = '%s_deconv_%dlayer_%inputs_%lr_%iters' % (os.path.splitext(os.path.basename(filename))[0],len(layers), inputs, learning_rate, iters)
+    else:
+        print 'building all convolutional cnn... {} layers, learning rate = {}, {} iters'.format(len(layers),learning_rate,iters)
 
-      if restore is not None:
-            print 'restoring model parameters from file...'
-            model.restore(restore)
-      else:
-          print 'training model...'
-          loss = model.train(data, iters, batch_size,inputs)
-          np.savetxt('%s/%s.loss' % (results_dir, model.name), loss.T, delimiter='\t', header='train\ttest', comments='')
+        name = '%s_conv_%dlayer_%inputs_%lr_%iters' % (os.path.splitext(os.path.basename(filename))[0],len(layers), inputs, learning_rate, iters)
 
-      print 'getting final predictions...'
-
-      testdata = data.sample(Ntest,axis=0)
-      indices = testdata.index.values
-     
-      if inputs == 2: 
-          all_donuts_x = np.vstack([np.expand_dims(x, 0) for x in testdata['psf'].values])
-          all_donuts_i = np.vstack([np.expand_dims(x, 0) for x in testdata['intra_psf'].values])
-          testDonut = np.stack((all_donuts_x,all_donuts_i),axis=-1)
-      else:
-          donuts = np.vstack([np.expand_dims(x, 0) for x in testdata['psf'].values])          
-          testDonut = np.expand_dims(donuts,-1) 
+    model = CNN(layers, name, learning_rate, batch_size, mask, inputs)
     
-      testWfPred = np.reshape(model.test(testDonut).flatten(),[-1,64,64])
+    if restore is not None:
+        print 'restoring model parameters from file...'
+        model.restore(restore)
+#    else:
+    print 'training model...'
+    loss = model.train(train_data, iters, batch_size,inputs)
+    np.savetxt('%s/%s.loss' % (results_dir, model.name), loss.T, delimiter='\t', header='train\ttest', comments='')
 
-      dfList = []    
-      errList = [] 
-      for i in range(len(indices)):
-          df = pd.DataFrame(columns = ['wavefront_pred'])
-          dfList.append( df.append({'wavefront_pred':testWfPred[i]},ignore_index=True) )
-          diff = testWfPred[i]-testdata['wavefront'].values[i]
-          errList.append(np.sum(np.square(diff)))
-      predDf = pd.concat(dfList, ignore_index=True)
-      testdata['wavefront_pred'] = predDf.values
+    print 'getting final predictions...'
 
-      test_err = np.sum(errList)/Ntest
-      
-      print 'final training error: %.4f' % test_err
 
-      testdata.to_pickle(results_dir + '/' + model.name + '_with_predictions.p')
+#    if inputs == 2: 
+#        all_donuts_x = np.vstack([np.expand_dims(x, 0) for x in test_data['psf'].values])
+#        all_donuts_i = np.vstack([np.expand_dims(x, 0) for x in test_data['intra_psf'].values])
+#        testDonut = np.stack((all_donuts_x,all_donuts_i),axis=-1)
+#    else:
+#        donuts = np.vstack([np.expand_dims(x, 0) for x in test_data['psf'].values])          
+#        testDonut = np.expand_dims(donuts,-1) 
+#    
+#    testWfPred = np.reshape(model.test(testDonut).flatten(),[-1,64,64])
 
-      if save:
-            print 'saving model parameters to file %s...' % model.name
-            model.save('_testerr%f' % test_err)
+### want to run next section to test both training and testing error 
+## would really love to find a better way of appending these predictions to my df.
+
+#    dfList = []    
+#    errList = [] 
+#    for i in range(Ntest):
+#        df = pd.DataFrame(columns = ['wavefront_pred'])
+#        dfList.append( df.append({'wavefront_pred':testWfPred[i]},ignore_index=True) )
+#        diff = testWfPred[i]-test_data['wavefront'].values[i]
+#        errList.append(np.sum(np.square(diff)))
+#    predDf = pd.concat(dfList, ignore_index=True)
+#    testdata['wavefront_pred'] = predDf.values
+#
+#    test_err = np.sum(errList)/Ntest
+
+    test_data, test_err = model.test_df(test_data,inputs)
+    train_test_data, train_err = model.test_df(train_test_data,inputs) 
+     
+    print 'final training error: %.4f' % train_err
+    print 'final testing error: %.4f' % test_err
+
+    testdata.to_pickle(results_dir + '/' + model.name + '_with_predictions.p')
+
+    if save:
+        print 'saving model parameters to file %s...' % model.name
+        model.save('_testerr%f' % test_err)
+#        import matplotlib.pyplot as plt
+          
+#        loss = np.loadtxt(basedir + directory + 'simulatedData_plus_' + name +'.loss',skiprows=1)
+
+#        pts = np.linspace(0,iters,len(loss))
+           
+        
 
 if __name__ == '__main__':
     main()
